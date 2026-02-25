2cd6f10d76f87baaeae7427db17ba3238e831c7a
Robin Huisma <robinhuisma@gmail.com>
2026-02-25 11:29:01 +0000

    Fix: preview-branches niet meer doorverwijzen naar geprompt.nl

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,7 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  if (url.hostname.endsWith('.pages.dev')) {
+  // Alleen de hoofddomein pages.dev doorverwijzen, NIET preview-branches
+  if (url.hostname === 'geprompt.pages.dev') {
     return Response.redirect('https://geprompt.nl' + url.pathname, 301);
   }
   return await context.next();
